01c5cff43f185e3949bc58f545007d4a17a18fee
Atreyu300 <45209190+Atreyu300@users.noreply.github.com>
2023-10-11 13:33:19 +0200

    set up basic of colony
    * At program initialization 5 bunnies must be created and given random colors.
    * Each turn afterwards the bunnies age 1 year.
    * So long as there is at least one male age 2 or older,
    * for each female bunny in the list age 2 or older;
    * New bunnies born should be the same color as their mother.
    * If a bunny becomes older than 10 years old, it dies.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <conio.h>
 #include <vector>
 #include <ctime>
 #include <chrono>
 #include <thread>
+
 
 using namespace std;
 
@@ -12,40 +14,31 @@ enum class EGender
 	Female
 };
 
+enum class EColor
+{
+	White,
+	Black,
+};
+
 struct Rabbit
 {
 	int Age;
 	EGender Gender;
+	EColor Color;
 
-	Rabbit() : Age(0), Gender(rand() % 2 == 0 ? EGender::Male : EGender::Female)
+	// Why is necessary to have a cast on Ecolor when is not necessary on EGeneder?
+	Rabbit() : Age(0), Gender(rand() % 2 == 0 ? EGender::Male : EGender::Female), 
+		Color(static_cast<EColor>(rand() % 2))
 	{
-		cout << "default ctor ";
+		cout << "";
 		// SendAnalytics("New Rabbit");
 	}
 
-	Rabbit(EGender Gender) : Age(0), Gender(Gender)
-	{}
-
-	Rabbit(const Rabbit& other) :
-		Age(other.Age),
-		Gender(other.Gender)
-	{
-		cout << "copy ctor ";
-	}
-
-	Rabbit& operator=(const Rabbit& other)
-	{
-		cout << "assignment op ";
-		Age = other.Age;
-		Gender = other.Gender;
-
-		return *this;
-	}
-
-	~Rabbit()
+	/*~Rabbit()
 	{
 		cout << "destruct ";
 	}
+	*/
 };
 
 struct World
@@ -55,27 +48,99 @@ struct World
 
 
 World* GWorld;
+int Turn;
 
 void Tick()
 {
-	cout << "tick tack ";
+	cout << "";
+}
+
+
+
+
+void IncreaseAnimalsAge() {
+
+	for (Rabbit& Rabbit : GWorld->RabbitColony) {
+		Rabbit.Age++;
+	}
+}
+
+bool CheckForMaleRabbit() {
+	for (const Rabbit& rabbit : GWorld->RabbitColony) {
+		if (rabbit.Gender == EGender::Male)
+			
+		return true;
+			else
+				return false;
+	}
+}
+
+void RabbitDeath(Rabbit) {
+	for (Rabbit& rabbit : GWorld->RabbitColony) {
+		{
+
+		}
+	}
+}
+
+void RabbitLifeCycle() 
+{
+
+	for (int i = 0; i < GWorld->RabbitColony.size(); ++i) 
+	{
+		Rabbit& rabbit = GWorld->RabbitColony[i];
+
+		if (rabbit.Age >= 10) 
+		{ 
+			GWorld->RabbitColony.erase(GWorld->RabbitColony.begin()+i);
+		}
+		if (bool malerabbit = CheckForMaleRabbit) 
+		{
+			if (rabbit.Age >= 2 && rabbit.Gender == EGender::Female) 
+			{
+				EColor BabyColor = rabbit.Color;
+				GWorld->RabbitColony.push_back(Rabbit()); 
+				// how to sets mom color in constructor?? 
+				// work around
+				Rabbit& RabbitToChangeColor = GWorld->RabbitColony.back();
+				RabbitToChangeColor.Color = BabyColor;
+													
+			}
+		}
+	}
+
+}
+	
+
+
+
+int NextTurn() {
+	Turn++;
+
+	IncreaseAnimalsAge();
+	
+	return Turn;
+}
+
+
+
+void StampRabbitColony() {
+
+	for (const Rabbit& Rabbit : GWorld->RabbitColony) {
+		cout << "Age: " << Rabbit.Age << ", Gender: " << (Rabbit.Gender == EGender::Male ? "Maschio" : "Femmina") << ", Colore: " << (Rabbit.Color == EColor::White ? "Bianco" : "Marrone") << std::endl;
+	}
 }
 
 int main()
 {
 	//InitAnalytics();
-	Rabbit r;
-	r.Gender = EGender::Female;
-
-	{
-		Rabbit r2(r);
-		Rabbit r3 = r;
-	}
-
-	Rabbit r4;
-	r4 = r;
 
 
+	char inputkey = 'a';
+
+
+
+	// Populating Colony
 	GWorld = new World();
 	GWorld->RabbitColony.push_back(Rabbit());
 	GWorld->RabbitColony.push_back(Rabbit());
@@ -83,12 +148,55 @@ int main()
 	GWorld->RabbitColony.push_back(Rabbit());
 	GWorld->RabbitColony.push_back(Rabbit());
 
+	// First Turn
+	//cout << "Turn number: " << Turn << "\n\n";
+	//StampRabbitColony();
+
+
+
+	// Timer Next Turn
+	while (true)
+	{
+		cout << "\n Press 'n' to move to the next turn";
+		inputkey = _getch();
+
+		if (inputkey == 'n') {
+
+
+			int TurnNumber = 0;
+			cout << "\n\nTurn number: " << TurnNumber << "\n";
+
+
+			RabbitLifeCycle();
+			StampRabbitColony();
+			TurnNumber = NextTurn();
+
+
+			// std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+
+		}
+	}
+
+
+
+
+
+
+	/*
 	while (true)
 	{
 		Tick();
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
 	}
+	*/
+
+
+
+
 
 	delete GWorld;
 	return 0;
+
 }
